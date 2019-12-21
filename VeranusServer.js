30b27adbf4e160251d6c8ac08a5e1b33ccd8fb77
Damon Ball <daball86@gmail.com>
2019-12-21 18:36:36 -0500

    Tweaking current server

--- VeranusServer.js
+++ VeranusServer.js
@@ -1,29 +1,155 @@
 var express = require('express'),
     http = require('http'),
-    fs = require('fs')
+    fs = require('fs'),
+    SerialPort = require('serialport')
 
 const PORT = 8001;
+const BAUD_RATE = 9600;
+const SERIAL = "COM5";
 
-var app = express();
-var server = http.createServer(app);
+const MSG_START = 0x7f;
+const MSG_END = 0xff;
 
-app.all('*', (req, res, next) =>
-{
-    sendFile(res, './index.html', 'text/html');
-})
+var activeSockets = [];
 
+const MAX_STORED_DATA = 1000;
+var savedData = [];
+var startIndex = 0;
+endIndex = 0;
+
+const app = express();
+const server = http.createServer(app);
 const io = require('socket.io')(server);
+const serialConnection = new SerialPort(
+    SERIAL,
+    {
+        baudRate: BAUD_RATE
+    }, 
+    (err) => {if(err) console.log(err)}
+);
 
-io.on("connect", (socket) =>
-{
-    console.log("Connected to " + socket.id);
-    socket.emit("ready", socket.id);
-})
+startServer();
+startSocket();
+startSerial();
 
-server.listen(PORT, () =>
+function startServer()
 {
-    console.log("Listening on port: " + PORT);
-});
+    app.get("/raw", (req, res) =>
+    {
+        sendRawData(res);
+    });
+    app.all('*', (req, res, next) =>
+    {
+        sendFile(res, './index.html', 'text/html');
+    });
+
+    server.listen(PORT, () =>
+    {
+        console.log("Listening on port: " + PORT);
+    });
+}
+
+function startSocket()
+{
+    io.on("connect", (socket) =>
+    {
+        console.log("Connected to " + socket.id);
+        activeSockets.push(socket);
+
+        socket.on("disconnect", () =>
+        {
+            console.log("Disconnected from " + socket.id);
+            for (var i=0; i<activeSockets.length; i++)
+            {
+                if(activeSockets[i].id == socket.id)
+                {
+                    activeSockets.splice(i, 1);
+                    break;
+                }
+            }
+        });
+    })
+}
+
+function startSerial()
+{
+    serialConnection.on("open", () =>
+    {
+        console.log("Serial connection opened on " + SERIAL);
+        serialConnection.on("data", (data) =>
+        {
+            parseSerial(data);
+        });
+    });
+}
+
+const EXPECTED_MESSAGE_LENGTH = 12;
+var buffer = [];
+var messageStart = false;
+function parseSerial(data)
+{
+    for (var i=0; i<data.length; i++)
+    {
+        if (messageStart)
+        {
+            if (data[i] == MSG_END ||
+                buffer.length > EXPECTED_MESSAGE_LENGTH)
+            {
+                if(buffer.length == EXPECTED_MESSAGE_LENGTH)
+                {
+                    handleNewData(bufferToMessage(buffer));
+                }
+                buffer = [];
+                messageStart = false;
+            }
+            else
+            {
+                buffer.push(data[i]);
+            }
+        }
+        else if (data[i] == MSG_START)
+        {
+            messageStart = true;
+        }
+    }
+}
+
+function handleNewData(msg)
+{
+    updateSockets(msg);
+    savedData[endIndex] = msg;
+    endIndex++;
+
+    if (endIndex >= MAX_STORED_DATA) endIndex = 0;
+    if (endIndex == startIndex) startIndex++;
+    if (startIndex >= MAX_STORED_DATA) startIndex = 0;
+}
+
+const VALUE_SIZE = 4;
+function bufferToMessage(buffer)
+{
+    var msg = {};
+    msg.timestamp = new Date().getTime();
+
+    msg.light = arrayToFloat32(buffer.slice(0, VALUE_SIZE));
+    msg.temp = arrayToFloat32(buffer.slice(VALUE_SIZE, VALUE_SIZE * 2));
+    msg.humid = arrayToFloat32(buffer.slice(VALUE_SIZE * 2, VALUE_SIZE * 3));
+
+    return msg;
+}
+
+function updateSockets(msg)
+{
+    activeSockets.map((socket) =>
+    {
+        socket.emit("update", msg);
+    })
+}
+
+function arrayToFloat32(arr)
+{
+    return new Float32Array(new Uint8Array(arr).buffer)[0]
+}
 
 function sendFile(res, filename, type)
 {
@@ -36,4 +162,30 @@ function sendFile(res, filename, type)
         res.end(content, 'utf-8');
 
     })
+}
+
+function sendRawData(res)
+{
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    if(savedData[startIndex] == undefined)
+    {
+        res.end("<body>No data yet.</body");
+        return;
+    } 
+    var startTime = savedData[startIndex].timestamp;
+    res.write("<div>Light, Temp, Humid, Timestamp</div>");
+    res.write("<body>");
+
+    var i=startIndex;
+    do{
+        var msg = savedData[i];
+        res.write("<div>" + msg.light + ',' + msg.temp + ',' + msg.humid + ',' + (msg.timestamp - startTime) + "</div>")
+        i++;
+        if (i >= MAX_STORED_DATA) i = 0;
+
+        console.log(savedData[i])
+        console.log(i + ',' + startIndex);
+    } while (savedData[i] != undefined &&
+            i != endIndex);
+    res.end("</body>", 'utf-8');
 }
